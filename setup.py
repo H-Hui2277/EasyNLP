ccf4f1ced77edd1921b128db828a01cf0d1f1737
H-Hui2277 <mrharry@hnu.edu.cn>
2023-09-08 11:08:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
+with open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     requirements = f.read()
 setup(
     # Metadata
